181d1aa8942bffaed97cfaadd47bb18774c07add
李阳 <1586557814@qq.com>
2016-08-29 10:26:14 +0800

    Initial Commit

---
+++ GWLAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GWLAnimationDemo
+//
+//  Created by liyangly on 16/8/29.
+//  Copyright © 2016年 liyangly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GWLAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GWLAnimationDemo
+//
+//  Created by liyangly on 16/8/29.
+//  Copyright © 2016年 liyangly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
